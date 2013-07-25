98c2738e08c5158c062eef3869140f7a38754a69
Scott Grogan <scott@ninjascript.com>
2013-07-24 22:11:25 -0700

    Adding enums: Teams, Positions, Players, Races, Actions, Abilities...

---
+++ enums.js
@@ -0,0 +1,75 @@
+module.exports = exports = {};
+
+/* ROSTERS 100 - 199 */
+exports.Teams = {};
+exports.Teams.TRONTEK_49ERS = 100;
+exports.Teams.GREENMOON_SMACKERS = 101;
+exports.Teams.SKITTERSNEAK_STEALERS = 102;
+exports.Teams.MIDGARD_DELVERS = 103;
+
+/* POSITIONS 200 - 299 */
+exports.Positions = {};
+exports.Positions.GUARD = 200;
+exports.Positions.JACK = 201;
+exports.Positions.STRIKER = 202;
+
+/* PLAYERS 300 - 399 */
+exports.Players = {};
+exports.Players.TRONTEK_49ERS_GUARD = 300;
+exports.Players.TRONTEK_49ERS_JACK = 301;
+exports.Players.TRONTEK_49ERS_STRIKER = 302;
+exports.Players.GREENMOON_SMACKERS_GUARD = 303;
+exports.Players.GREENMOON_SMACKERS_JACK = 304;
+exports.Players.SKITTERSNEAK_STEALERS_GUARD = 305;
+exports.Players.SKITTERSNEAK_STEALERS_STRIKER = 306;
+exports.Players.MIDGARD_DELVERS_GUARD = 307;
+exports.Players.MIDGARD_DELVERS_JACK = 308;
+exports.Players.MIDGARD_DELVERS_STRIKER = 309;
+
+/* RACES 600 - 699 */
+exports.Races = {};
+exports.Races.HUMAN = 600;
+exports.Races.ORX = 601;
+exports.Races.GOBLIN = 602;
+exports.Races.VEERMYN = 603;
+exports.Races.FORGEFATHER = 604;
+
+/* ACTIONS 400 - 499 */
+exports.Actions = {};
+exports.Actions.RUN = 400;
+exports.Actions.SPRINT = 401;
+exports.Actions.DASH = 402;
+exports.Actions.PICK_UP = 403;
+exports.Actions.THROW = 404;
+exports.Actions.CATCH = 405;
+exports.Actions.EVADE = 406;
+exports.Actions.DODGE = 407;
+exports.Actions.SLAM = 408;
+exports.Actions.STEAL = 409;
+exports.Actions.SLAMBACK = 410;
+exports.Actions.STOMP = 411;
+exports.Actions.DISTRACT_THE_REF = 412;
+exports.Actions.MOVE_ONTO_THE_PITCH = 413;
+exports.Actions.ARMOR_CHECK = 414;
+exports.Actions.STAND_UP = 415;
+exports.Actions.SUCKER_PUNCH = 416;
+exports.Actions.JUMP = 417;
+exports.Actions.PUNT = 418;
+exports.Actions.ROLL = 419;
+
+/* ABILITIES 500 - 599 */
+exports.Abilities = {};
+exports.Abilities.SAFE_PAIR_OF_HANDS = 500;
+exports.Abilities.BACKFLIP = 501;
+exports.Abilities.CANT_FEEL_A_THING = 502;
+exports.Abilities.DOES_THIS_HURT = 503;
+exports.Abilities.GOTCHA = 504;
+exports.Abilities.GRIZZLED = 505;
+exports.Abilities.JUMP = 506;
+exports.Abilities.KEEPER = 507;
+exports.Abilities.LUCKY = 508;
+exports.Abilities.REALLY_LUCKY = 509;
+exports.Abilities.MIND_LIKE_WATER = 510;
+exports.Abilities.ROLL = 511;
+exports.Abilities.RUNNING_INTERFERENCE = 512;
+exports.Abilities.STEADY = 513;
