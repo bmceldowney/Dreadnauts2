4324bedfc55e487961733c64d4b29c352ad9a048
Scott Grogan <scott@ninjascript.com>
2013-07-26 23:00:46 -0700

    Playing around with front-end animations

---
+++ app/public/javascripts/main.js
@@ -0,0 +1,16 @@
+$('body').hammer().on('touch', 'button.action', function(evt) {
+    $(this).addClass('on');
+});
+
+$('body').hammer().on('drag swipe', 'button.action', function(evt) {
+    evt.gesture.preventDefault();
+});
+
+$('body').hammer().on('release', 'button.action', function(evt) {
+    
+    var target = $(this);
+
+    target.addClass('rock').one('transitionend webkitTransitionEnd oTransitionEnd', function() {
+        target.css('visibility', 'hidden');
+    });
+});
