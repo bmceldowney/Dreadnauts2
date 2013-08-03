d2f34b67eabe23c57ce6729de1449218b9ac8c98
Scott Grogan <scott@ninjascript.com>
2013-08-02 18:22:24 -0700

    Reorganizing the tests so that the utility methods come after the actual test cases

--- test/builders/players.js
+++ test/builders/players.js
@@ -4,6 +4,69 @@ var Player = require('../../app/models/player');
 var Players = require('../../app/builders/players');
 var should = require('should');
 
+describe('TronTek 49ers Guard', function() {
+    var subject = Players.build(Enums.Players.TRONTEK_49ERS_GUARD);
+    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.GUARD, 5, 4, 4, 4, 4);
+});
+
+describe('TronTek 49ers Jack', function() {
+    var subject = Players.build(Enums.Players.TRONTEK_49ERS_JACK);
+    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.JACK, 5, 4, 4, 4, 4);
+});
+
+describe('TronTek 49ers Striker', function() {
+    var subject = Players.build(Enums.Players.TRONTEK_49ERS_STRIKER);
+    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.STRIKER, 5, 4, 4, 4, 5);
+});
+
+describe('Greenmoon Smackers Guard', function() {
+    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_GUARD);
+    testPlayer(subject, Enums.Races.ORX, Enums.Positions.GUARD, 5, 3, 4, 5, 4);
+});
+
+describe('Greenmoon Smackers Jack', function() {
+    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_JACK);
+    testPlayer(subject, Enums.Races.GOBLIN, Enums.Positions.JACK, 5, 5, 3, 4, 4);
+});
+
+describe('Greenmoon Smackers Striker', function() {
+    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_STRIKER);
+    testPlayer(subject, null);
+});
+
+describe('Skittersneak Stealers Guard', function() {
+    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_GUARD);
+    testPlayer(subject, Enums.Races.VEERMYN, Enums.Positions.GUARD, 6, 4, 3, 5, 4);
+});
+
+describe('Skittersneak Stealers Jack', function() {
+    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_JACK);
+    testPlayer(subject, null);
+});
+
+describe('Skittersneak Stealers Striker', function() {
+    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_STRIKER);
+    testPlayer(subject, Enums.Races.VEERMYN, Enums.Positions.STRIKER, 6, 4, 3, 5, 5);
+});
+
+describe('Midgard Delvers Guard', function() {
+    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_GUARD);
+    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.GUARD, 4, 3, 5, 4, 4);
+    it('should have the "steady" ability', function() {
+        subject.abilities.should.include(Enums.Abilities.STEADY);
+    });
+});
+
+describe('Midgard Delvers Jack', function() {
+    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_JACK);
+    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.JACK, 4, 3, 5, 4, 4);
+});
+
+describe('Midgard Delvers Striker', function() {
+    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_STRIKER);
+    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.STRIKER, 4, 3, 5, 4, 5);
+});
+
 function testPlayer(subject, race, position, move, strength, speed, skill, armor) {
 
     if (race == null) {
@@ -103,91 +166,3 @@ function testPlayerStats(subject, move, strength, speed, skill, armor) {
         subject.armor.should.equal(armor);
     });
 };
-
-describe('TronTek 49ers Guard', function() {
-
-    var subject = Players.build(Enums.Players.TRONTEK_49ERS_GUARD);
-    
-    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.GUARD, 5, 4, 4, 4, 4);
-});
-
-describe('TronTek 49ers Jack', function() {
-    
-    var subject = Players.build(Enums.Players.TRONTEK_49ERS_JACK);
-    
-    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.JACK, 5, 4, 4, 4, 4);
-});
-
-describe('TronTek 49ers Striker', function() {
-    
-    var subject = Players.build(Enums.Players.TRONTEK_49ERS_STRIKER);
-    
-    testPlayer(subject, Enums.Races.HUMAN, Enums.Positions.STRIKER, 5, 4, 4, 4, 5);
-});
-
-describe('Greenmoon Smackers Guard', function() {
-    
-    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_GUARD);
-    
-    testPlayer(subject, Enums.Races.ORX, Enums.Positions.GUARD, 5, 3, 4, 5, 4);
-});
-
-describe('Greenmoon Smackers Jack', function() {
-    
-    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_JACK);
-    
-    testPlayer(subject, Enums.Races.GOBLIN, Enums.Positions.JACK, 5, 5, 3, 4, 4);
-});
-
-describe('Greenmoon Smackers Striker', function() {
-    
-    var subject = Players.build(Enums.Players.GREENMOON_SMACKERS_STRIKER);
-    
-    testPlayer(subject, null);
-});
-
-describe('Skittersneak Stealers Guard', function() {
-    
-    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_GUARD);
-    
-    testPlayer(subject, Enums.Races.VEERMYN, Enums.Positions.GUARD, 6, 4, 3, 5, 4);
-});
-
-describe('Skittersneak Stealers Jack', function() {
-    
-    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_JACK);
-    
-    testPlayer(subject, null);
-});
-
-describe('Skittersneak Stealers Striker', function() {
-    
-    var subject = Players.build(Enums.Players.SKITTERSNEAK_STEALERS_STRIKER);
-    
-    testPlayer(subject, Enums.Races.VEERMYN, Enums.Positions.STRIKER, 6, 4, 3, 5, 5);
-});
-
-describe('Midgard Delvers Guard', function() {
-    
-    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_GUARD);
-    
-    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.GUARD, 4, 3, 5, 4, 4);
-    
-    it('should have the "steady" ability', function() {
-        subject.abilities.should.include(Enums.Abilities.STEADY);
-    });
-});
-
-describe('Midgard Delvers Jack', function() {
-    
-    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_JACK);
-    
-    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.JACK, 4, 3, 5, 4, 4);
-});
-
-describe('Midgard Delvers Striker', function() {
-    
-    var subject = Players.build(Enums.Players.MIDGARD_DELVERS_STRIKER); 
-    
-    testPlayer(subject, Enums.Races.FORGEFATHER, Enums.Positions.STRIKER, 4, 3, 5, 4, 5);
-});
--- test/builders/teams.js
+++ test/builders/teams.js
@@ -5,34 +5,6 @@ var Player = require('../../app/models/player');
 var Team = require('../../app/models/team');
 var should = require('should');
 
-function testTeam(team, expectations) {
-
-    var tally = {};
-
-    it('should be a Team', function() {
-        team.should.be.an.instanceOf(Team);
-    });
-
-    team.players.forEach(function(player) {
-        tally[player.position + player.race] = tally[player.position + player.race] + 1 || 1;
-    });
-
-    expectations.forEach(function(set) {
-
-        if (set.count > 0) {
-            it('should have %1 %2 %3(s)'.replace('%1', set.count).replace('%2', Constants[set.race]).replace('%3', Constants[set.position]), function() {
-                should.exist(tally[set.position + set.race]);
-                tally[set.position + set.race].should.equal(set.count);
-            });
-        }
-        else {
-            it('should have 0 %1s'.replace('%1', Constants[set.position]), function() {
-                should.not.exist(tally[set.position + set.race]);
-            });
-        }
-    });
-};
-
 describe('TronTek 49ers', function() {
     testTeam(Teams.build(Enums.Teams.TRONTEK_49ERS), [
         { position: Enums.Positions.GUARD, count: 2, race: Enums.Races.HUMAN },
@@ -64,3 +36,31 @@ describe('Midgard Delvers', function() {
         { position: Enums.Positions.STRIKER, count: 2, race: Enums.Races.FORGEFATHER },
     ]);
 });
+
+function testTeam(team, expectations) {
+
+    var tally = {};
+
+    it('should be a Team', function() {
+        team.should.be.an.instanceOf(Team);
+    });
+
+    team.players.forEach(function(player) {
+        tally[player.position + player.race] = tally[player.position + player.race] + 1 || 1;
+    });
+
+    expectations.forEach(function(set) {
+
+        if (set.count > 0) {
+            it('should have %1 %2 %3(s)'.replace('%1', set.count).replace('%2', Constants[set.race]).replace('%3', Constants[set.position]), function() {
+                should.exist(tally[set.position + set.race]);
+                tally[set.position + set.race].should.equal(set.count);
+            });
+        }
+        else {
+            it('should have 0 %1s'.replace('%1', Constants[set.position]), function() {
+                should.not.exist(tally[set.position + set.race]);
+            });
+        }
+    });
+};
